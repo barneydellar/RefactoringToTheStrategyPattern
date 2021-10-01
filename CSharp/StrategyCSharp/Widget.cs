36b6dd745ff77febc060acc0777da8cfb2705a83
Barnaby Dellar <Barnaby.Dellar@eu.medical.canon>
2021-10-01 14:29:40 +0100

    Removing Shape, as it was redundant.

--- CSharp/StrategyCSharp/Widget.cs
+++ CSharp/StrategyCSharp/Widget.cs
@@ -34,32 +34,24 @@ namespace StrategyCSharp
 
         public void Draw(ICanvas canvas)
         {
+            canvas.DrawVertex(new Point(0, 0));
+            canvas.DrawVertex(new Point(2, 0));
+            if (_type == Type.Triangle)
+            {
+                canvas.DrawVertex(new Point(1, 2));
+            }
+            else
+            {
+                canvas.DrawVertex(new Point(0, 2));
+                canvas.DrawVertex(new Point(2, 2));
+            }
             switch (_type)
             {
-                case Type.Triangle:
-                    canvas.DrawVertex(new Point(0, 0));
-                    canvas.DrawVertex(new Point(2, 0));
-                    canvas.DrawVertex(new Point(1, 2));
-                    break;
-                case Type.Square:
-                    canvas.DrawVertex(new Point(0, 0));
-                    canvas.DrawVertex(new Point(0, 2));
-                    canvas.DrawVertex(new Point(2, 2));
-                    canvas.DrawVertex(new Point(2, 0));
-                    break;
                 case Type.Pentagon:
-                    canvas.DrawVertex(new Point(0, 0));
-                    canvas.DrawVertex(new Point(2, 0));
-                    canvas.DrawVertex(new Point(2, 2));
                     canvas.DrawVertex(new Point(1, 3));
-                    canvas.DrawVertex(new Point(0, 2));
                     break;
                 case Type.Hexagon:
-                    canvas.DrawVertex(new Point(0, 0));
-                    canvas.DrawVertex(new Point(2, 0));
                     canvas.DrawVertex(new Point(3, 1));
-                    canvas.DrawVertex(new Point(2, 2));
-                    canvas.DrawVertex(new Point(0, 2));
                     canvas.DrawVertex(new Point(-1, 1));
                     break;
             }
--- cppVS/Strategy/Widget.cpp
+++ cppVS/Strategy/Widget.cpp
@@ -21,31 +21,24 @@ std::string Widget::Describe() const
 
 void Widget::Draw(ICanvas& canvas) const
 {
-    switch (m_type) {
-    case Type::Triangle:
-        canvas.DrawVertex({ 0, 0 });
-        canvas.DrawVertex({ 2, 0 });
+
+
+    canvas.DrawVertex({ 0, 0 });
+    canvas.DrawVertex({ 2, 0 });
+    if (m_type == Type::Triangle) {
         canvas.DrawVertex({ 1, 2 });
-        break;
-    case Type::Square:
-        canvas.DrawVertex({ 0, 0 });
+    }
+    else {
         canvas.DrawVertex({ 0, 2 });
         canvas.DrawVertex({ 2, 2 });
-        canvas.DrawVertex({ 2, 0 });
-        break;
+    }
+
+    switch (m_type) {
     case Type::Pentagon:
-        canvas.DrawVertex({ 0, 0 });
-        canvas.DrawVertex({ 2, 0 });
-        canvas.DrawVertex({ 2, 2 });
         canvas.DrawVertex({ 1, 3 });
-        canvas.DrawVertex({ 0, 2 });
         break;
     case Type::Hexagon:
-        canvas.DrawVertex({ 0, 0 });
-        canvas.DrawVertex({ 2, 0 });
         canvas.DrawVertex({ 3, 1 });
-        canvas.DrawVertex({ 2, 2 });
-        canvas.DrawVertex({ 0, 2 });
         canvas.DrawVertex({ -1, 1 });
         break;
     }
